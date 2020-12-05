06676743da19eda4aa890ed9f31a138e230f99bb
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-05 14:39:43 -0600

    move elements to HeroGroup

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn to design and code React apps</h1>
-    <p>
-      Complete courses about the best tools and design systems. Prototype and
-      build apps with React and Swift.
-    </p>
-    <Link to="/page-2/">Watch the video</Link> <br />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>
+          Complete courses about the best tools and design systems. Prototype
+          and build apps with React and Swift.
+        </p>
+        <Link to="/page-2/">Watch the video</Link> <br />
+      </div>
+    </div>
   </Layout>
 )
 
