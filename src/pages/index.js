627812846bb1355e2b6eaaec96ff9de44b04ca80
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-05 13:58:25 -0600

    remove image from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -13,9 +12,6 @@ const IndexPage = () => (
       Complete courses about the best tools and design systems. Prototype and
       build apps with React and Swift.
     </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Watch the video</Link> <br />
   </Layout>
 )
