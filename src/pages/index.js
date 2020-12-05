885a7f213c1308fdfef2389615573b8c053e5029
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-05 11:26:32 -0600

    change p text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Learn to design and code React apps</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>
+      Complete courses about the best tools and design systems. Prototype and
+      build apps with React and Swift.
+    </p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
