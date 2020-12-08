4660b1a81827ebb7dc2562ae7b27a9164e1be942
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-08 10:48:45 -0600

    add alt property to Card img

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import "./Card.css"
 
 const Card = ({ title, text, image }) => (
   <div className="Card">
-    <img src={image} />
+    <img src={image} alt="" />
     <h3>{title}</h3>
     <p>{text}</p>
   </div>
