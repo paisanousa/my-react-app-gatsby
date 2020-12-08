985d4ac66f2b45620d9aab759072d09e51eab05e
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-08 10:42:08 -0600

    style Cards to adaptable grid, add gradient to title

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,11 +50,28 @@ const IndexPage = () => (
     </div>
     <div className="Cards">
       <h2>11 courses, more coming</h2>
-      <Card
-        title="React for Designers"
-        text="12 sections"
-        image={require("../images/wallpaper.jpg")}
-      />
+      <div className="CardGroup">
+        <Card
+          title="Design System"
+          text="10 sections"
+          image={require("../images/wallpaper.jpg")}
+        />
+        <Card
+          title="React for Designers"
+          text="12 sections"
+          image={require("../images/wallpaper2.jpg")}
+        />
+        <Card
+          title="Sound Design"
+          text="5 sections"
+          image={require("../images/wallpaper3.jpg")}
+        />
+        <Card
+          title="ARKit 2"
+          text="10 sections"
+          image={require("../images/wallpaper4.jpg")}
+        />
+      </div>
     </div>
   </Layout>
 )
