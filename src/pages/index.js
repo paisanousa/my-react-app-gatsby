07bbaf397638e42e3d7668de34c2733b41ef58e0
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-09 22:09:25 -0600

    add CellGroup w logos and titles

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import SEO from "../components/SEO"
 import Card from "../components/Card"
 import Section from "../components/Section"
 import Wave from "../components/Wave"
+import staticdata from "../../staticdata.json"
+import Cell from "../components/Cell"
 
 const IndexPage = () => (
   <Layout>
@@ -63,6 +65,9 @@ const IndexPage = () => (
       title="React for Designers"
       text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
     />
+    {staticdata.cells.map(cell => (
+      <Cell title={cell.title} image={cell.image} />
+    ))}
   </Layout>
 )
 
